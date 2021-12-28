283d9aed7a37341282ae60c4cd2b98ac6cf53379
Hessam <s.hesam.ghasemi@gmail.com>
2021-12-28 20:52:05 +0330

    Before removing redux

--- ui/src/store.js
+++
@@ -1,6 +0,0 @@
-import {applyMiddleware, createStore} from 'redux';
-import thunk from 'redux-thunk'; //allows us to use functions as actions
-import Reducers from './containers/App/reducers';
-
-const store = createStore(Reducers, applyMiddleware(thunk));
-export default store;
--- ui/src/websocket.js
+++ ui/src/websocket.js
@@ -15,12 +15,6 @@ export function startWebsocket() {
         console.log('websocket message event:', e)
     }
 
-    // ws.onclose = function () {
-    //     console.debug('websocket "onclose"')
-    //     // connection closed, discard old websocket and create a new one in 5s
-    //     ws = null
-    //     setTimeout(startWebsocket, WEBSOCKET_RECONNECT_INTERVAL_IN_MS)
-    // }
     return ws
 }
 
