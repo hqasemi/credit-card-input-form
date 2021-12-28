8dcecfb81ee02ce620cf0af46071537927d9e427
Hessam <s.hesam.ghasemi@gmail.com>
2021-12-28 21:16:30 +0330

    app listens to EXPRESS_SERVER_PORT

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ const app = express();
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "./ui/build")));
-app.listen(80, () =>
+app.listen(EXPRESS_SERVER_PORT, () =>
     console.log(`Example app listening on port ${EXPRESS_SERVER_PORT}!`),
 );
 //  ----------------------------------------------------------------------------------------------------------
