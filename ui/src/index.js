2e656d3af5b9f7ee5f44ea338fbc9791b2ff663c
Hessam <s.hesam.ghasemi@gmail.com>
2021-12-28 21:30:47 +0330

    Fix a problem in ui code

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
-import {Provider} from 'react-redux';
-import store from './store';
-// import App from "./containers/App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            {/*<App/>*/}
-            <App/>
-        </Provider>
+        <App/>
     </React.StrictMode>,
     document.getElementById('root')
 );
